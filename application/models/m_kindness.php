d5e45e57b1413c37b884c5696d535228c9ba3351
VoRD <vord.cs@gmail.com>
2014-07-20 01:40:55 +0700

    kindness validation + get post form add edit non img

--- application/models/m_kindness.php
+++ application/models/m_kindness.php
@@ -83,12 +83,12 @@ Class m_kindness extends CI_Model {
             'name' => 'kindness_content',
             'id' => 'content',
             'class' => 'form-control',
-            'value' => set_value('kindness_content')
+            'value' => (set_value('kindness_content') == NULL) ? $data['kindness_content'] : set_value('kindness_content')
         );
         $f_img = array(
             'name' => 'kindness_img',
 //            'class' => 'form-control',
-            'value' => set_value('kindness_img')
+            'value' => (set_value('kindness_img') == NULL) ? $data['kindness_img'] : set_value('kindness_img')
         );
 
 
@@ -97,7 +97,7 @@ Class m_kindness extends CI_Model {
             'class' => 'form-control',
             'id' => 'publish_date',
             'placeholder' => '',
-            'value' => set_value('publish_date')
+            'value' => (set_value('publish_date') == NULL) ? $data['publish_date'] : set_value('publish_date')
         );
         $f_highlight = array(
             '0' => 'ทั่วไป',
@@ -109,14 +109,14 @@ Class m_kindness extends CI_Model {
             '1' => 'ไม่ใช้งาน',
         );
 
-        $form_add = array(
-            'form' => form_open_multipart('Kindness_ad/add', array('class' => 'form-horizontal', 'id' => 'form_kindness')),
+        $form_edit = array(
+            'form' => form_open_multipart('Kindness_ad/edit' .$data['kindness_id'], array('class' => 'form-horizontal', 'id' => 'form_kindness')),
             'kindness_title' => form_input($f_kindness_title),
             'kindness_content' => form_textarea($f_kindness_content),
             'kindness_img' => form_upload($f_img),
             //            ''=>  form_input($f_),          
             'kindness_status' => form_dropdown('kindness_status', $f_status, (set_value('kindness_status') == NULL) ? $data['kindness_status'] : set_value('kindness_status'), 'class="form-control"'),
-            'kindness_highlight' => form_dropdown('kindness_highlight', $f_highlight, set_value('kindness_highlight'), 'class="form-control"'),
+            'kindness_highlight' => form_dropdown('kindness_highlight', $f_highlight, (set_value('kindness_highlight')== NULL) ? $data['kindness_highlight'] : set_value('kindness_highlight'), 'class="form-control"'),
             'publish_date' => form_input($f_publish_date),
             'image' => NULL,
         );
@@ -126,27 +126,38 @@ Class m_kindness extends CI_Model {
 
     public function validation_add() {
         $this->form_validation->set_rules('kindness_title', 'ชื่อเรื่อง', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('kindness_content', 'เนื้อหา', 'required|trim|xss_clean');
         if (empty($_FILES['kindness_imge']['name'])) {
             $this->form_validation->set_rules('kindness_img', 'รูปภาพ', 'required|xss_clean');
         }
+        $this->form_validation->set_rules('publish_date', 'วันที่', 'required|trim|xss_clean');
         return TRUE;
     }
 
     public function validation_edit() {
         $this->form_validation->set_rules('kindness_title', 'ชื่อเรื่อง', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('kindness_content', 'เนื้อหา', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('publish_date', 'วันที่', 'required|trim|xss_clean');
         return TRUE;
     }
 
     public function get_post_form_add() {
         $page_data = array(
             'kindness_title'=>$this->input->post('kindness_title'),
+            'kindness_content'=>$this->input->post('kindness_content'),
+            'kindness_status'=>$this->input->post('kindness_status'),
+            'kindness_highlight'=>$this->input->post('kindness_highlight'),
 //            ''=>$this->input->post(''),
         );
         return $page_data;
     }
-        public function get_post_form_edit() {
+        public function get_post_form_edit($id) {
         $page_data = array(
             'kindness_title'=>$this->input->post('kindness_title'),
+            'kindness_content'=>$this->input->post('kindness_content'),
+            'kindness_status'=>$this->input->post('kindness_status'),
+            'kindness_highlight'=>$this->input->post('kindness_highlight'),
+            
 //            ''=>$this->input->post(''),
         );
         return $page_data;
