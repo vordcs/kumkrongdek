436d276323c12ac6da3145ef32ab6384b111b6c2
araart <AZTT_@Rachatas-MacBook-Air.local>
2014-07-23 02:52:26 +0700

    foot

--- application/views/theme_footer.php
+++ application/views/theme_footer.php
@@ -2,8 +2,86 @@
     <li><a title="กลับด้านบน" style="color:#222;"><i class="fa fa-angle-double-up fa-lg"></i></a></li>
 </ul>
 <div class="footer"> 
-<div class="footer_bg">    
-        <div class="container">
+    <div class="footer_top">
+        <div class="container hidden-xs">
+            <div class="row" align="center">
+                <div class="col-xs-6 col-md-2">
+                    <a href="www.google.com" >
+                <?php 
+                    $img_propr = array(
+                        'class'=>'img-responsive',
+                        'width'=>'50%'
+                    )
+                    ?>
+                    <?=  img('/foot_logo/logo1.gif',$img_propr); ?>
+                        กระทรวงการพัฒนาสังคมและความมั่นคงของมนุษย์
+                    </a>
+                </div>
+                <div class="col-xs-6 col-md-2">
+                    <a href="www.google.com" >
+                <?php 
+                    $img_propr = array(
+                        'class'=>'img-responsive',
+                        'width'=>'50%'
+                    )
+                    ?>
+                    <?=  img('/foot_logo/logo2.gif',$img_propr); ?>
+                        กรมพัฒนาสังคมและสวัสดิการ
+                    </a></div>
+                <div class="col-xs-6 col-md-2">
+                    <a href="www.google.com" >
+                <?php 
+                    $img_propr = array(
+                        'class'=>'img-responsive',
+                        'width'=>'50%'
+                    )
+                    ?>
+                    <?=  img('/foot_logo/logo3.gif',$img_propr); ?>
+                        สำนักคุ้มครองสวัสดิภาพหญิงและเด็ก
+                    </a></div>
+                <div class="col-xs-6 col-md-2">
+                    <a href="www.google.com" >
+                <?php 
+                    $img_propr = array(
+                        'class'=>'img-responsive',
+                        'width'=>'50%'
+                    )
+                    ?>
+                    <?=  img('/foot_logo/logo1.gif',$img_propr); ?>
+                        ศูนย์ประชาบดี  1300
+                    </a></div>
+                <div class="col-xs-6 col-md-2">
+                    <a href="www.google.com" >
+                <?php 
+                    $img_propr = array(
+                        'class'=>'img-responsive',
+                        'width'=>'50%'
+                    )
+                    ?>
+                    <?=  img('/foot_logo/logo1.gif',$img_propr); ?>
+                         สหกรณออมทรพย์  สป.พม.สหกรณออมทรพย์  สป.พม.
+                    </a></div>
+                <div class="col-xs-6 col-md-2">
+                    <a href="http://www.khonkaen.go.th" >
+                <?php 
+                    $img_propr = array(
+                        'class'=>'img-responsive',
+                        'width'=>'50%'
+                    )
+                    ?>
+                    <?=  img('/foot_logo/logo6.gif',$img_propr); ?>
+                        จังหวัดขอนแก่น
+                    </a></div>
+            </div>
+
+        </div>      
+
+    </div>
+    <div class="footer_mid" align="center">
+        สถานคุ้มครองสวัสดิภาพเด็กภาคตะวันออกเฉียงเหนือ
+    </div>
+    <div class="footer_bg">    
+        <div class="container">          
             <div class="footer_bottom">
                 <p>Page Generation: {elapsed_time} &copy;</p>
             </div>
