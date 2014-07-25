af1ebd8e532ee5bdb80bcb912c9d0e7c5349671d
VoRD <vord.cs@gmail.com>
2014-07-25 17:26:01 +0700

    improve

--- application/controllers/Activitys.php
+++ application/controllers/Activitys.php
@@ -23,4 +23,45 @@ class Activitys extends CI_Controller {
         $this->m_template->showTemplate();
     }
 
+    public function view_more($id) {
+
+
+        $activitys = $this->m_activitys->get_activitys($id);
+        $images_activity = $this->m_activitys->get_image_activity();
+
+        $data['images'] = NULL;
+        $controller = "Activitys";
+        $activitys = $this->m_activitys->get_activitys($id);
+        foreach ($activitys as $row) {
+            $img = $row['image_small'];
+            $title = $row['activity_title'];
+            $subtitle = $row['activity_subtitle'];
+            $content = $row['activity_content'];
+            $date = $this->m_datetime->DateThai($row['publish_date']);
+            $status = $row['activity_status'];
+            $create = '  | สร้าง : ' . $this->m_datetime->DateTimeThai($row['create_date']) . ' โดย: ' . $row['create_by'];
+            $update = 'แก้ไข : ' . $this->m_datetime->DateTimeThai($row['update_date']) . ' โดย: ' . $row['update_by'];
+        }
+
+        $data = array(
+            'controller' => $controller,
+            'img' => $img,
+            'id' => $id,
+            'title' => $title,
+            'subtitle' => $subtitle,
+            'date' => $date,
+            'status' => $status,
+            'content' => $content,
+            'create' => $create,
+            'update' => $update,
+        );
+
+        $data['images'] = $this->m_activitys->get_image_activity($id);
+//
+//        $this->m_template->set_Debug($kindness);
+        $this->m_template->set_Title('รายละเอียด : ' . $title);
+        $this->m_template->set_Content('detail.php', $data);
+        $this->m_template->showTemplate();
+    }
+
 }
--- application/views/detail.php
+++ application/views/detail.php
@@ -3,59 +3,57 @@ $close = array(
     'type' => "button",
     'class' => "btn btn-success",
 );
-
-$view_more = array(
-    'type' => "button",
-    'class' => "btn btn-link pull-right",
-    'style' => "font-size: 0.91em;",
-);
 ?>  
+<div class="title_bg">
+    <div class="row">
+        <div class="top_main" > </div>
+    </div>  
+</div>
+
 <div class="row content">
-    <div class="col-xs-12 col-md-8 col-md-offset-2" style="padding: 2%">   
-        <div class="col-sm-4">
-            <a class="fancybox" rel="gallery<?= $id ?>" href="<?= img_url() . $img ?>">
-                <?= img($img, array('class' => 'img-responsive ui large left floated image')); ?>
-            </a> 
-        </div>
-        <div class="col-sm-8">
-            <div class="row"> 
+    <div class="container" >
+        <div class="row" style="padding-top: 4%">
+            <div class="col-sm-2">
 
-     <!--<img class="ui small left floated image" data-src="holder.js/100x100/sky" width="100px" height="100px">-->                        
-
-                <div style="font-size: 1.5em;"><?= $title ?></div>
-                <div class="text-muted"><small><?= $date ?></small></div>                    
-                <div class="description">                                   
-                    <p>
-                        <?= $content ?>                                 
-                    </p>  
-                </div>
             </div>
-        </div>
-        <?php
-        if (count($images) > 0) {
-            ?>
-            <div class="row" id="gallery" style="margin-top: 3%;" > 
-                <h3><strong>รูปภาพอื่นๆ</strong></h3>
+            <div class="col-sm-8">
+                <div class="row">
+                    <a class="fancybox" rel="gallery<?= $id ?>" href="<?= img_url() . $img ?>">
+                        <?= img($img, array('class' => 'img-responsive ui small right floated image')); ?>
+                    </a> 
+
+                    <div class="content">
+                        <h1 class="ui header">   
+                            <span class="name"><?= $title ?></span>                        
+                            <div class="text-muted"><?= $date ?></div> 
+                        </h1>  
+                        <div class="description text">
+                            <?= $content ?>    
+                        </div>
+                    </div>  
+                </div>               
                 <?php
-                foreach ($images as $row_img) {
-                    echo '<a class="fancybox" rel="gallery' . $id . '" href="' . img_url() . $row_img['image_full'] . '"><img src="' . img_url() . $row_img['image_full'] . '"alt="..." width="100%" ></a>';
-                }
-                ?>  
-            </div>
-        <?php } ?>
-        <div class="panel panel-default">  
-            <div class="panel-body"> 
-
-            </div>              
-            <div class="panel-footer">
-                <p class="text-center">     
-                    <?php
-                    echo '<span class="icon">' . anchor($controller, 'ปิด', $close) . '</span>';
+                if (count($images) > 0) {
                     ?>
-                </p>
+                    <div class="row" id="gallery" style="margin-top: 3%;" > 
+                        <h3><strong>รูปภาพอื่นๆ</strong></h3>
+                        <?php
+                        foreach ($images as $row_img) {
+                            echo '<a class="fancybox" rel="gallery' . $id . '" href="' . img_url() . $row_img['image_full'] . '"><img src="' . img_url() . $row_img['image_full'] . '"alt="..." width="100%" ></a>';
+                        }
+                        ?>  
+                    </div>
+                <?php }
+                ?>
+
+
+
             </div>
+            <div class="col-sm-2"></div>        
         </div>
     </div>
 </div>
+
+
 
 
